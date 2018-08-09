5edcf52dbfe061aa8bfd67047297546d63d535ec
Rodolfo Abarca <r9dolfinho@hotmail.com>
2018-08-09 18:26:55 -0500

    Updated footer on datatable

--- src/PageSizeSelect.vue
+++ src/PageSizeSelect.vue
@@ -1,26 +1,28 @@
 <template>
-  {{query.limit}} records of {{total}} total records -
-  <label name="PageSizeSelect">
-    <select class="form-control input-sm -page-size-select" v-model="query.limit"
-      @change="query.offset = 0 /* reset to the first page */">
-      <option v-for="i in pageSizeOptions" :value="i">{{ i }}</option>
-    </select>
-    {{ $i18nForDatatable('items / page') }}
-  </label>
+    <div class="col-sm-6" style="white-space: nowrap">
+        {{query.limit}} records of {{total}} total records -
+        <label name="PageSizeSelect">
+            <select class="form-control input-sm -page-size-select" v-model="query.limit"
+                    @change="query.offset = 0 /* reset to the first page */">
+                <option v-for="i in pageSizeOptions" :value="i">{{ i }}</option>
+            </select>
+            {{ $i18nForDatatable('items / page') }}
+        </label>
+    </div>
 </template>
 <script>
-export default {
-  name: 'PageSizeSelect',
-  props: {
-    query: { type: Object, required: true },
-    pageSizeOptions: { type: Array, required: true },
-    total:0,
-  }
-}
+    export default {
+        name: 'PageSizeSelect',
+        props: {
+            query: {type: Object, required: true},
+            pageSizeOptions: {type: Array, required: true},
+            total: 0,
+        }
+    }
 </script>
 <style>
-.-page-size-select {
-  display: inline-block;
-  width: 65px;
-}
+    .-page-size-select {
+        display: inline-block;
+        width: 65px;
+    }
 </style>
